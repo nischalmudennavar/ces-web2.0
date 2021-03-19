c1791dbafa450103e6a597e5e86fa94114c5c8fb
nischalmudennavar <nischalmudennavar@gmail.com>
2021-03-19 19:08:11 +0530

    major changes

--- js/app.main.js
+++ js/app.main.js
@@ -1,5 +1,8 @@
 const loadAnimationCes=()=>{
 
+
+    
+
     gsap.registerPlugin(ScrollTrigger);
     gsap.from('.hi',{
         scale:0,
@@ -35,5 +38,31 @@ const loadAnimationCes=()=>{
         stagger:0.1,
         delay:3,
     })
+
+    
+var modal = document.getElementById("modal-1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("event-modal-1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 }
